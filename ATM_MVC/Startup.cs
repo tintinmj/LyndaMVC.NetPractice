8115b6525bddc25e962801a0e98f8704f787eed7
tintinmj <anirban.nag9038541773@gmail.com>
2014-12-21 02:13:25 +0530

    First commit

---
+++ ATM_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ATM_MVC.Startup))]
+namespace ATM_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
